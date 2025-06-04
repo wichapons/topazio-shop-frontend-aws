52c52819a24c83c27c4c41d7348930fd1b5fd734
Wichapon Viriyatharangkurn <wichapon@gmail.com>
2025-06-04 21:13:36 +0700

    chore: Update API proxy target to use environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: '3000',
     proxy: {
       '/api': {
-        target: "https://topazio-shop-backend.onrender.com",
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, '')
       }
